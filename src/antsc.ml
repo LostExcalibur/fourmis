095631c431586d0fe3f15e9e0756bd9010353316
LostExcalibur <romaindebeaucorps@yahoo.fr>
2022-10-19 14:50:47 +0200

    Possibilité de passer le nom de sortie en argument

--- src/antsc.ml
+++ src/antsc.ml
@@ -1,22 +1,9 @@
 open Printf
 
-let write_file filename = (* Ceci est un exemple. *)
-  let oc = open_out filename in (* Ouvre un fichier pour écrire dedans. *)
-  let write_label (msg:string) : unit = (* Écriture d'un label. *)
-    fprintf oc "%s:\n" msg in
-  let write_command (msg:string) : unit = (* Écriture d'autres commandes. *)
-    fprintf oc "  %s\n" msg in
-  write_label   "start";
-  write_command "Drop";
-  write_command "Goto start";
-  close_out oc
-
 type macro = string * (Ast.expression list)
-
 
 let i = ref 0
 let macros: macro list ref = ref [] 
-let oc = open_out "cervo.brain"
 
 
 let macro_existe (m: string) : bool = 
@@ -26,7 +13,7 @@ let macro_existe (m: string) : bool =
     in parcours !macros
 
 
-let ajouter_macro (m: macro) = 
+let ajouter_macro (m: macro) : unit = 
     match m with 
     | (nom, _) when macro_existe nom -> failwith "Macro déjà définie"
     | _ -> macros := (m)::(!macros) 
@@ -44,7 +31,7 @@ let unwrap_expr (e: Ast.expression CodeMap.Span.located) : Ast.expression =
     | (expr, _) -> expr 
 
 
-let comp_direction dir =
+let comp_direction (dir: Ast.direction) : string =
     match dir with
         | Ast.Here ->               "Here"
         | Ast.Left ->               "LeftAhead"
@@ -52,13 +39,13 @@ let comp_direction dir =
         | Ast.Ahead ->              "Ahead"
 
 
-let comp_lr dir =
+let comp_lr (dir: Ast.lr) : string =
     match dir with
         | Ast.TurnL ->              "Left"
         | Ast.TurnR ->              "Right"
  
 
-let comp_valeur valeur =
+let comp_valeur (valeur: Ast.valeur) : string =
     match valeur with
         | Ast.Friend ->             "Friend"
         | Ast.Foe ->                "Foe"
@@ -71,7 +58,7 @@ let comp_valeur valeur =
         | Ast.Home ->               "Home"
         | Ast.FoeHome ->            "FoeHome"
 
-let comp_command commande =
+let comp_command (commande: Ast.command) (oc: out_channel) : unit =
     match commande with
         | Ast.Nope ->               ()
         | Ast.Move ->
@@ -85,7 +72,7 @@ let comp_command commande =
         | Ast.Drop ->               fprintf oc "  Drop \n"
 
 
-let comp_condition cond c =
+let comp_condition (cond: Ast.condition) (c: int) (oc: out_channel) : unit =
     match cond with
         | Ast.Eq((direction,_),(valeur,_)) ->       
             fprintf oc "  Sense %s label_%d label_%d %s\n" (comp_direction direction) (c) (c+1) (comp_valeur valeur)
@@ -95,27 +82,28 @@ let comp_condition cond c =
                         fprintf oc "  Flip %d label_%d label_%d\n" (p) (c) (c+1)
 
 
-let rec comp_expression exp =
+let rec comp_expression (exp: Ast.expression) (oc: out_channel) : unit =
     match exp with
-        | Ast.Do(commande,_) -> comp_command commande
-        | Ast.IfThenElse((cond,_),(exp1,_),(exp2,_)) -> 
-                        let c = !i in i := c + 3;
-                        comp_condition cond c;
+        | Ast.Do(commande,_) -> comp_command commande oc
+        | Ast.IfThenElse((cond,_),(exp1,_),(exp2,_)) ->
+            let comp_with_out = (fun x -> comp_expression x oc) in begin
+                let c = !i in i := c + 3;
+                        comp_condition cond c oc;
                         fprintf oc "label_%d:\n" c;
-                        List.iter comp_expression (List.map fst exp1);
+                        List.iter comp_with_out (List.map unwrap_expr exp1);
                         fprintf oc "  Goto label_%d\n" (c+2);
                         fprintf oc "label_%d:\n" (c+1);
-                        List.iter comp_expression (List.map fst exp2);
+                        List.iter comp_with_out (List.map unwrap_expr exp2);
                         fprintf oc "  Goto label_%d\n" (c+2);
                         fprintf oc "label_%d:\n" (c+2)
-            
+            end
         | Ast.While((cond,_),(exp,_)) ->
                         let c = !i in i := c + 3;
                         fprintf oc "  Goto label_%d\n" c;
                         fprintf oc "label_%d:\n" c;
-                        comp_condition cond (c+1);
+                        comp_condition cond (c+1) oc;
                         fprintf oc "label_%d:\n" (c+1);
-                        List.iter comp_expression (List.map fst exp);
+                        List.iter (fun x -> comp_expression x oc) (List.map fst exp);
                         fprintf oc "  Goto label_%d\n" c;
                         fprintf oc "label_%d:\n" (c+2)
         | Ast.Macro((nom, _), (liste, _)) -> begin 
@@ -124,43 +112,46 @@ let rec comp_expression exp =
             else
                 ajouter_macro (nom, (List.map unwrap_expr liste))
         end
-        | Ast.Call(nom, _) -> if not (macro_existe nom) then failwith "BaTaMakroExistePa" else let (_, instructions) = trouver_macro nom in List.iter comp_expression instructions 
+        | Ast.Call(nom, _) -> if not (macro_existe nom) then failwith "BaTaMakroExistePa" else let (_, instructions) = trouver_macro nom in List.iter (fun x -> comp_expression x oc) instructions 
 
 
-let comp_program program =
+let comp_program (program: Ast.program) (oc: out_channel) : unit =
     match program with
         | Ast.Program(expression_l,_) ->
                 (* TODO : Refactor possible en List.map unwrap_expr puis List.iter comp_expression *)
                 let rec aux l =
                     match l with
                         |[] -> ()
-                        |(exp,_)::q -> comp_expression exp ; aux q
+                        |(exp,_)::q -> comp_expression exp oc; aux q
                 in
                 fprintf oc "debut:\n";
                 aux (expression_l);
                 fprintf oc "  Goto debut\n"
                 
 
-let process_file filename =
+let process_file (filename: string) (output: string) : unit =
   (* Ouvre le fichier et créé un lexer. *)
-  let file = open_in filename in
-  let lexer = Lexer.of_channel file in
+    let file = open_in filename and out = open_out output in
+    let lexer = Lexer.of_channel file in
   (* Parse le fichier. *)
-  let (program, span) = Parser.parse_program lexer in
-  printf "successfully parsed the following program at position %t:\n%t\n" (CodeMap.Span.print span) (Ast.print_program program);
-  comp_program program
+    let (program, span) = Parser.parse_program lexer in
+    printf "successfully parsed the following program at position %t:\n%t\n" (CodeMap.Span.print span) (Ast.print_program program);
+    comp_program program out
+
 
 (* Le point de départ du compilateur. *)
 let _ =
+  let argc = Array.length Sys.argv in 
   (* On commence par lire le nom du fichier à compiler passé en paramètre. *)
-  if Array.length Sys.argv <= 1 then begin
+  if argc <= 2 then begin
     (* Pas de fichier... *)
-    eprintf "no file provided.\n";
+    eprintf "Usage :\n\t<%s> entree sortie\n" Sys.argv.(0);
     exit 1
   end else begin
     try
       (* On compile le fichier. *)
-      process_file (Sys.argv.(1))
+      process_file Sys.argv.(1) (if argc = 3 then Sys.argv.(2) else "cervo.brain")
+
     with
     | Lexer.Error (e, span) ->
       eprintf "Lex error: %t: %t\n" (CodeMap.Span.print span) (Lexer.print_error e)
