5034a1570ce3a48c838f8e88318f6d2f0e4d03d1
LostExcalibur <romaindebeaucorps@yahoo.fr>
2022-10-20 17:40:51 +0200

    Optimisation pour diminuer le nombre de lignes, pour l'instant ça conserve le fichier pas opti

--- src/antsc.ml
+++ src/antsc.ml
@@ -1,4 +1,5 @@
 open Printf
+open Str
 
 type macro = string * (Ast.expression list)
 
@@ -53,7 +54,7 @@ let comp_valeur (valeur: Ast.valeur) : string =
         | Ast.FoeWithFood ->        "FoeWithFood"
         | Ast.Food ->               "Food"
         | Ast.Rock ->               "Rock"
-        | Ast.Marker(i,_) ->        "Marker "^(string_of_int i)
+        | Ast.Marker(i,_) ->        "Marker " ^ (string_of_int i)
         | Ast.FoeMarker ->          "FoeMarker"
         | Ast.Home ->               "Home"
         | Ast.FoeHome ->            "FoeHome"
@@ -161,14 +162,8 @@ let rec comp_expression (exp: Ast.expression) (oc: out_channel) : unit =
 let comp_program (program: Ast.program) (oc: out_channel) : unit =
     match program with
         | Ast.Program(expression_l,_) ->
-                (* TODO : Refactor possible en List.map unwrap_expr puis List.iter comp_expression *)
-                let rec aux l =
-                    match l with
-                        |[] -> ()
-                        |(exp,_)::q -> comp_expression exp oc; aux q
-                in
                 fprintf oc "debut:\n";
-                aux (expression_l);
+                List.iter (fun x -> comp_expression x oc) (List.map unwrap_expr expression_l);
                 fprintf oc "  Goto debut\n"
                 
 
@@ -179,8 +174,58 @@ let process_file (filename: string) (output: string) : unit =
   (* Parse le fichier. *)
     let (program, _) = Parser.parse_program lexer in
     (* printf "successfully parsed the following program at position %t:\n%t\n" (CodeMap.Span.print span) (Ast.print_program program); *)
-    comp_program program out
+    comp_program program out;
+    close_out out
 
+let post_replace (motif: string) (nouveau: string) : string -> string = 
+    Str.global_replace (Str.regexp motif) nouveau 
+
+let post_trouver_labels_inutiles (nom: string) : (string * string) list  = 
+    let ic = open_in nom and label_regex = Str.regexp "\([a-zA-Z][a-zA-Z0-9_]*\):" and goto_regex = Str.regexp "Goto \([a-zA-Z][a-zA-Z0-9_]*\)" and result = ref [] in
+    try while true do
+        let ligne = input_line ic in 
+        (* On a un label de défini, on regarde si la prochaine ligne est un goto *)
+        if Str.string_match label_regex ligne 0 then
+            let prochaine_ligne = input_line ic and label_dp = (matched_group 1 ligne) in
+            (* On a un label suivi d'un goto, on va pouvoir opti *)
+            if Str.string_match goto_regex prochaine_ligne 2 then
+                result := (label_dp, matched_group 1 prochaine_ligne)::!result
+    done; 
+    !result
+    with
+    End_of_file -> !result
+
+let rec post_mauvais_label (nom: string) (data: (string * string) list) : bool = match data with
+    | [] -> false
+    | (n, _)::q -> n = nom || post_mauvais_label nom q 
+
+
+let post_regexp_string (data: (string * string) list) : string = 
+  let rec aux l = match l with
+    [] -> ""
+  | (label1_nom1, label1_nom2)::[label2_nom1, label2_nom2] -> label1_nom1 ^ ":\n  Goto " ^ label1_nom2 ^ "\n\|" ^ label2_nom1 ^ ":\n  Goto " ^ label2_nom2 ^ "\n"
+  | (nom1, nom2)::q -> nom1 ^ ":\n  Goto " ^ nom2 ^ "\n\|" ^ (aux q)
+  in "\(" ^ (aux data) ^ "\)"
+
+let rec post_print_labels (data: (string * string) list) : unit = 
+  match data with
+    [] -> ()
+  | (nom1, nom2)::q -> printf "%s:\n  %s\n" nom1 nom2; post_print_labels q
+
+
+let post_remplacer_labels (data: (string * string) list) (filename_out: string) (filename_opti: string) : unit = 
+  let ic = open_in filename_out in
+  let content = In_channel.really_input_string ic (in_channel_length ic) in 
+  close_in ic; 
+  let rec aux l s = match l with
+      [] -> ()
+    | (nom1, nom2)::q -> s := post_replace (nom1 ^ "$") nom2 !s (* ; printf "%s\n\n" !s *); aux q s
+  and contenu = ref (post_replace (post_regexp_string data) "" (Option.get content)) and out_file = open_out filename_opti in begin
+    (* post_print_labels (List.rev data); *)
+    aux (List.rev data) contenu;
+    fprintf out_file "%s" !contenu;
+    close_out out_file
+  end
 
 (* Le point de départ du compilateur. *)
 let _ =
@@ -193,7 +238,11 @@ let _ =
   end else begin
     try
       (* On compile le fichier. *)
-      process_file Sys.argv.(1) (if argc = 3 then Sys.argv.(2) else "cervo.brain")
+      let name = if argc = 3 then Sys.argv.(2) else "cervo.brain" in
+      process_file Sys.argv.(1) name;
+      let post_test_filename = name in
+      let labels = post_trouver_labels_inutiles post_test_filename in
+      post_remplacer_labels labels post_test_filename "opti.brain";
 
     with
     | Lexer.Error (e, span) ->
